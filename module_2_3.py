6f8855d9b38cd17cbf94e1437fc4edcf8fd85bbb
Olga <glazkovaolga_78@mail.ru>
2024-08-13 03:10:20 +0500

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,10 +1,11 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-b = -1
-while b <= len(my_list):
-    b = b + 1
+b = 0
+while b < len(my_list):
     if my_list[b] > 0:
         print(my_list[b])
+        b += 1
     elif my_list[b] == 0:
+        b += 1
         continue
     else:
         break
